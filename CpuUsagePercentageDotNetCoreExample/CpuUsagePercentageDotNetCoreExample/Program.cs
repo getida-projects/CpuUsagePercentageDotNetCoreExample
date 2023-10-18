a4f92b6716fda8f7f0c1d3c4ddeee07c5a6a420d
AzKurban2 <azkurban@gmail.com>
2023-10-18 07:40:15 +0300

    CPU Monitor Test App implemented.

--- CpuUsagePercentageDotNetCoreExample/CpuUsagePercentageDotNetCoreExample/Program.cs
+++ CpuUsagePercentageDotNetCoreExample/CpuUsagePercentageDotNetCoreExample/Program.cs
@@ -1,23 +1,60 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace CpuUsagePercentageDotNetCoreExample
 {
+    internal class CpuUsageTime
+    {
+        public TimeSpan StartTime { get; set; }
+        public TimeSpan EndTime { get; set; }
+    }
+
     class Program
     {
         public static async Task Main(string[] args)
         {
-            var task = Task.Run(() => ConsumeCPU(50));
+            //var task = Task.Run(() => ConsumeCPU(50));
 
-            while (true)
+            try
             {
-                await Task.Delay(2000);
-                var cpuUsage = await GetCpuUsageForProcess();
+                Process[] processes = Process.GetProcesses()
+                    .Where(p => p.ProcessName != "Idle" && !p.HasExited)
+                    .ToArray();
+
+                //Console.WriteLine($"Currently {processes.Length} processes is active");
+
+                double cpuUsage = await GetCpuUsage(processes);
 
                 Console.WriteLine(cpuUsage);
+
+
+                while (true)
+                {
+                    await Task.Delay(2000);
+
+                    processes = Process.GetProcesses()
+                        .Where(p => p.ProcessName != "Idle" && !p.HasExited)
+                        .ToArray();
+
+                    //Console.WriteLine($"Currently {processes.Length} processes is active");
+
+                    var newCpuUsage = await GetCpuUsage(processes);
+                    cpuUsage = (cpuUsage + newCpuUsage) / 2;
+
+                    Console.WriteLine(cpuUsage);
+                }
             }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.ToString());
+            }
+
+
         }
 
         public static void ConsumeCPU(int percentage)
@@ -35,22 +72,58 @@ namespace CpuUsagePercentageDotNetCoreExample
             }
         }
 
-        private static async Task<double> GetCpuUsageForProcess()
+        private static async Task<double> GetCpuUsage(Process[] processes)
         {
+            Dictionary<Process, CpuUsageTime> cpuUsageMap = new(); 
+
             var startTime = DateTime.UtcNow;
-            var startCpuUsage = Process.GetCurrentProcess().TotalProcessorTime;
+
+            foreach (Process process in processes)
+            {
+                if (process.HasExited)
+                {
+                    Console.WriteLine($"Process {process.ProcessName} (ID: {process.Id} has exited.");
+                    continue;
+                }
+
+                cpuUsageMap[process] = new CpuUsageTime
+                {
+                    StartTime = process.TotalProcessorTime
+                };
+            }
+
+            var survivedProcesses = cpuUsageMap.Keys.ToArray();
 
             await Task.Delay(500);
 
             var endTime = DateTime.UtcNow;
-            var endCpuUsage = Process.GetCurrentProcess().TotalProcessorTime;
+            var totalMsPassed = (endTime - startTime).TotalMilliseconds * Environment.ProcessorCount;
 
-            var cpuUsedMs = (endCpuUsage - startCpuUsage).TotalMilliseconds;
-            var totalMsPassed = (endTime - startTime).TotalMilliseconds;
+            foreach (Process process in survivedProcesses)
+            {
+                if (process.HasExited)
+                {
+                    cpuUsageMap.Remove(process);
 
-            var cpuUsageTotal = cpuUsedMs / (Environment.ProcessorCount * totalMsPassed);
+                    Console.WriteLine($"Process {process.ProcessName} (ID: {process.Id} has exited.");
 
-            return cpuUsageTotal * 100;
+                    continue;
+                }
+
+                cpuUsageMap[process].EndTime = process.TotalProcessorTime;
+            }
+
+            TimeSpan cpuTotalUsage = new();
+
+            foreach (Process process in cpuUsageMap.Keys)
+            {
+                CpuUsageTime cpuUsageTime = cpuUsageMap[process];
+
+                cpuTotalUsage += cpuUsageTime.EndTime - cpuUsageTime.StartTime;
+            }
+
+
+            return cpuTotalUsage.TotalMilliseconds /totalMsPassed * 100;
         }
     }
 }
